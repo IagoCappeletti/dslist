0ef51818f2b7f3ce5f1cca742086ebce6cd0fb1a
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-07-13 14:49:29 -0300

    SQL, projection, get games by list

--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.example.dslist.services;
 import java.util.List;
 
 import com.example.dslist.dto.GameDTO;
+import com.example.dslist.projections.GameMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,10 +24,18 @@ public class GameService {
 		return new GameDTO(result);
 	}
 
+	/*Chama o gameRepository.findAll que busca t0do mundo do
+	banco de dados e devolve um lista de Obj e converti cada Obj para o GameMinDTO.*/
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
-		List<Game> result = gameRepository.findAll(); //Chama o gameRepository.findAll que busca t0do mundo do
-		// banco de dados e devolve um lista de Obj e converti cada Obj para o GameMinDTO.
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+
+	/*Pegando a lista projection, convertendo com a função map para uma list de GameMinDTO*/
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listID){
+		List<GameMinProjection> result = gameRepository.searchByList(listID);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
